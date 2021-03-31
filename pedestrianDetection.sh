14e39756f57fc875538bcbc56ca98b0c9564a269
rakshith-kumar-j <rakshithkumarj@gmail.com>
2021-03-31 10:36:17 +0530

    Third Commit

--- pedestrianDetection.sh
+++ pedestrianDetection.sh
@@ -1,3 +1,6 @@
+wget https://download.01.org/opencv/2021/openvinotoolkit/2021.2/open_model_zoo/models_bin/3/pedestrian-detection-adas-0002/FP16/pedestrian-detection-adas-0002.bin
+
+wget https://download.01.org/opencv/2021/openvinotoolkit/2021.2/open_model_zoo/models_bin/3/pedestrian-detection-adas-0002/FP16/pedestrian-detection-adas-0002.xml
 
 cd ~/openvino/bin/armv7l/Release
 
